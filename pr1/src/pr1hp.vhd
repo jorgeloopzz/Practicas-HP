12591a011f7c943b6aa418acea4e5d6384a86f8d
jorgeloopzz <jorgeloopzz@protonmail.com>
2025-02-14 08:02:42 +0000

    Declaro los componentes de las otras instancias

--- pr1/src/pr1hp.vhd
+++ pr1/src/pr1hp.vhd
@@ -13,7 +13,6 @@ library ieee;
   USE ieee.numeric_std.all;
   USE WORK.ALL;
   
-
 entity pr1hp is
   port (
     clk100 : in std_logic;
@@ -23,3 +22,42 @@ entity pr1hp is
     sseg   : out std_logic_vector(7 downto 0)
   );
 end pr1hp;
+
+architecture top of pr1hp is
+
+  component sseg2disp is
+    port (
+      clk100 : in std_logic;
+      rst_n  : in std_logic;
+      sseg0  : in std_logic_vector(7 downto 0);
+      sseg1  : in std_logic_vector(7 downto 0);
+      sseg2  : in std_logic_vector(7 downto 0);
+      sseg3  : in std_logic_vector(7 downto 0);
+      sseg4  : in std_logic_vector(7 downto 0);
+      sseg5  : in std_logic_vector(7 downto 0);
+      sseg6  : in std_logic_vector(7 downto 0);
+      sseg7  : in std_logic_vector(7 downto 0);
+      an     : out std_logic_vector(7 downto 0);
+      sseg   : out std_logic_vector(7 downto 0)
+    );
+  end component;
+
+  component bcd2sseg is
+    port (
+      bcd  : in std_logic_vector(3 downto 0);
+      sseg : out std_logic_vector(7 downto 0)
+    );
+  end component;
+
+  component bin2bcd is
+    port (
+      bin   : in std_logic_vector(7 downto 0);
+      bcd_u : out std_logic_vector(3 downto 0);
+      bcd_d : out std_logic_vector(3 downto 0);
+      bcd_c : out std_logic_vector(3 downto 0)
+    );
+  end component;
+
+begin
+
+end architecture;
