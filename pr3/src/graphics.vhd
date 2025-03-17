a3250a0766bf11f44aba66e6680e4ad3b94989bf
jorgeloopzz <jorgeloopzz@protonmail.com>
2025-03-17 17:05:51 +0000

    Generación de la nave

--- pr3/src/graphics.vhd
+++ pr3/src/graphics.vhd
@@ -33,3 +33,44 @@ entity graphics is
     b             : out std_logic_vector (3 downto 0)
   );
 end graphics;
+
+architecture funcional of graphics is
+  signal ship_rom_enable : std_logic;
+  signal ship_relative_x : std_logic_vector(10 downto 0);
+  signal ship_relative_y : std_logic_vector(10 downto 0);
+  signal ship_addr       : std_logic_vector(10 downto 0);
+
+begin
+
+  ------------------------------------------------------
+  -- Generación de la nave
+  -------------------------------------------------------
+  ship_rom_enable <= '1' when
+    ((unsigned(px_x) >= unsigned(ship_x)) and
+    (unsigned(px_x) < unsigned(ship_x) + to_unsigned(SHIP_WIDTH, 11))) and
+    ((unsigned(px_y) >= unsigned(ship_y)) and
+    (unsigned(px_y) < unsigned(ship_y) + to_unsigned(SHIP_HEIGHT, 11)))
+    else
+    '0';
+
+  ship_relative_x <= std_logic_vector(unsigned(px_x) - unsigned(ship_x)) when
+    (ship_rom_enable = '1') else
+    (others => '0');
+
+  ship_relative_y <= std_logic_vector(unsigned(px_y) - unsigned(ship_y)) when
+    (ship_rom_enable = '1') else
+    (others => '0');
+
+  ship_addr <= ship_relative_y(5 downto 0) & ship_relative_x(5 downto 0);
+
+  -- Componente de la nave
+  ship_object : ship_object_rom
+  port map
+  (
+    clka  => clk100,
+    ena   => ship_rom_enable,
+    addra => ship_addr,
+    douta => ship_rom
+  );
+
+end architecture;
